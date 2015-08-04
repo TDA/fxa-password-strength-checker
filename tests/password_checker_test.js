f75b7ca3c7ce8ba4920456442be91fc4d95afb40
Sai Pc <schand31@asu.edu>
2015-08-04 13:16:32 -0700

    Added grunt-mocha

--- tests/password_checker_test.js
+++ tests/password_checker_test.js
@@ -2,33 +2,36 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-(function() {
-  /*global chai, passwordcheck */
-  'use strict';
-  var assert = chai.assert;
-  describe('Password validation with regex', function () {
-    it('returns MISSING_PASSWORD when no password is passed', function () {
-      passwordcheck('', function (res) {
-        assert.equal('MISSING_PASSWORD', res);
+define(['chai', 'passwordcheck'], function (chai, PasswordCheck) {
+  (function() {
+    var assert = chai.assert;
+    var passwordcheck = new PasswordCheck();
+    'use strict';
+    describe('Password validation with regex', function () {
+      it('returns MISSING_PASSWORD when no password is passed', function () {
+        passwordcheck('', function (res) {
+          assert.equal('MISSING_PASSWORD', res);
+        });
       });
-    });
 
-    it('returns PASSWORD_TOO_SHORT when password is lower than minLength', function () {
-      passwordcheck('124as', function (res) {
-        assert.equal('PASSWORD_TOO_SHORT', res);
+      it('returns PASSWORD_TOO_SHORT when password is lower than minLength', function () {
+        passwordcheck('124as', function (res) {
+          assert.equal('PASSWORD_TOO_SHORT', res);
+        });
       });
-    });
 
-    it('returns ALL_LETTERS_NUMBERS when password is all numbers', function () {
-      passwordcheck('12456789', function (res) {
-        assert.equal('ALL_NUMBERS_LETTERS', res);
+      it('returns ALL_LETTERS_NUMBERS when password is all numbers', function () {
+        passwordcheck('12456789', function (res) {
+          assert.equal('ALL_NUMBERS_LETTERS', res);
+        });
       });
-    });
 
-    it('returns ALL_LETTERS_NUMBERS when password is all letters', function () {
-      passwordcheck('dragondrag', function (res) {
-        assert.equal('ALL_NUMBERS_LETTERS', res);
+      it('returns ALL_LETTERS_NUMBERS when password is all letters', function () {
+        passwordcheck('dragondrag', function (res) {
+          assert.equal('ALL_NUMBERS_LETTERS', res);
+        });
       });
     });
-  });
-})();
+  })();
+
+});
